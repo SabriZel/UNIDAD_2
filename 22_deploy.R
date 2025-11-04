43993d51c34c65e757c411b9ec3e30caa45f48c7
SabriZel <sabridospuntostres@gmail.com>
2025-11-04 16:14:13 -0400

    Añadir archivo

--- 22_deploy.R
+++ 22_deploy.R
@@ -4,10 +4,11 @@ library(rsconnect)
 rsconnect::setAccountInfo(name='sabrizel0',
                           token='20A6B3B62D16053F260100F9D111ECA1',
                           secret='79eUh9kb09vaamtQimNeAI7tLtTsh0ekFJNImAIW')
-rsconnect::deployApp(appDir = "C://Users/sabri/OneDrive/Documentos/4TO_SEM/Estadistica_IV/Unidad2/21_shiny",
+rsconnect::deployApp(appDir = "C://Users/sabri/OneDrive/Documentos/4TO_SEM/Estadistica_IV/Unidad2/22_shiny",
                      
-                     appName = "21_myapp")
+                     appName = "22_myapp")
 
 
-#rsconnect::showLogs(appName = "21_myapp")
+#rsconnect::showLogs(appName = "22_myapp")
+
 
